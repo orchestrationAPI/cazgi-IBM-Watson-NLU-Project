d81d1d121a45456ff81108b9e4884f1a7ab4bd8f
IBM <49950247+orchestrationAPI@users.noreply.github.com>
2021-04-01 16:47:14 +0530

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+function getNLUInstance() {
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2020-08-01',
+  authenticator: new IamAuthenticator({
+    apikey: api_key,
+  }),
+  serviceUrl: api_url,
+});
+ return naturalLanguageUnderstanding;
+}
 const app = new express();
 
 app.use(express.static('client'))
