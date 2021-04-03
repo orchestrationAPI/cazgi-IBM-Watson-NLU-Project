d2b3a8afafcc3bb91b988adb6da14974bbd3e210
IBM <49950247+orchestrationAPI@users.noreply.github.com>
2021-04-03 22:13:28 +0530

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,18 +1,4 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-var getNLUInstance = function getNLUInstance() {
-const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-  version: '2020-08-01',
-  authenticator: new IamAuthenticator({
-    apikey: api_key,
-  }),
-  serviceUrl: api_url,
-});
- return naturalLanguageUnderstanding;
-}
 const app = new express();
 
 app.use(express.static('client'))
@@ -25,26 +11,22 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-     
-    return res.send(getNLUInstance());
+
+    return res.send({"happy":"90","sad":"10"});
 });
 
 app.get("/url/sentiment", (req,res) => {
-    
     return res.send("url sentiment for "+req.query.url);
 });
 
 app.get("/text/emotion", (req,res) => {
-   
-    return res.send(getNLUInstance());
+    return res.send({"happy":"10","sad":"90"});
 });
 
 app.get("/text/sentiment", (req,res) => {
-   
     return res.send("text sentiment for "+req.query.text);
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
