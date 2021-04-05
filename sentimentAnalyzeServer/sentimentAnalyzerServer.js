d7b3562bfc347dc780f4f7e6fbd6fe4c54bdc5e1
IBM <49950247+orchestrationAPI@users.noreply.github.com>
2021-04-05 11:34:34 +0530

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -14,16 +14,7 @@ let api_url = process.env.API_URL;
   
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
-const analyzeParams = {
-  'url': 'www.ibm.com',
-  'features': {
-    'keywords': {
-      'sentiment': true,
-      'emotion': true,
-      'limit': 3
-    }
-  }
-};  
+
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2020-08-01',
   authenticator: new IamAuthenticator({
@@ -32,27 +23,20 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   serviceUrl: api_url,
 });
   
- naturalLanguageUnderstanding.analyze(analyzeParams)
-  .then(analysisResults => {
-    console.log(JSON.stringify(analysisResults, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  }); 
- return naturalLanguageUnderstanding;
-}
+  return naturalLanguageUnderstanding;
+}  
 
 
 
 
 
 app.get("/",(req,res)=>{
-    res.render();
+    res.render('app.js');
   });
 
 app.get("/url/emotion", (req,res) => {
-   return getNLUInstance();
-   // return res.send({"happy":"90","sad":"10"});
+  
+    return res.send(getNLUInstance());
   
 });
 
@@ -61,8 +45,7 @@ app.get("/url/sentiment", (req,res) => {
 });
 
 app.get("/text/emotion", (req,res) => {
-   return getNLUInstance();
-   // return res.send({"happy":"10","sad":"90"});
+    return res.send(getNLUInstance());
 });
 
 app.get("/text/sentiment", (req,res) => {
