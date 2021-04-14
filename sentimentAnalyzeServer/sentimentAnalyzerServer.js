3fe322ce80ce93b32205cb660fea8e5e8a4072ba
IBM <49950247+orchestrationAPI@users.noreply.github.com>
2021-04-14 10:16:56 +0530

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = new express();
+const path = require('path');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
+
+function getNLUInstance() {
+let api_key = process.env.API_KEY;
+let api_url = process.env.API_URL;
+  
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2020-08-01',
+  authenticator: new IamAuthenticator({
+    apikey: api_key,
+  }),
+  serviceUrl: api_url,
+});
+  
+  return naturalLanguageUnderstanding;
+}  
 
 app.get("/",(req,res)=>{
     res.render('index.html');
@@ -26,6 +48,8 @@ app.get("/text/emotion", (req,res) => {
 app.get("/text/sentiment", (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
+
+app.listen(port, () => console.log('Listening on port ${port}'));
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
