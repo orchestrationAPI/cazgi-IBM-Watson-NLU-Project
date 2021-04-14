9e300d32c144f98b0c88d9f8e04219a02d072e4e
IBM <49950247+orchestrationAPI@users.noreply.github.com>
2021-04-14 07:41:02 +0530

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,38 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = new express();
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
-
-function getNLUInstance() {
-let api_key = process.env.API_KEY;
-let api_url = process.env.API_URL;
-  
-const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-  version: '2020-08-01',
-  authenticator: new IamAuthenticator({
-    apikey: api_key,
-  }),
-  serviceUrl: api_url,
-});
-  
-  return naturalLanguageUnderstanding;
-}  
-
-
 
 app.get("/",(req,res)=>{
     res.render('index.html');
@@ -54,18 +26,6 @@ app.get("/text/emotion", (req,res) => {
 app.get("/text/sentiment", (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'sentimentAnalyzeClient/src')));
-    
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'sentimentAnalyzeClient/src', 'index.js'));
-  });
-}
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
